9ea620d1852900b8cebfd1e7214a4b337fad2cd6
János Szomor <jszomor@organicawater.com>
2020-02-18 22:45:41 +0100

    init

---
+++ Stringek/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stringek
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string szovegresz;
+			string osszszoveg = "";
+			while(true)
+			{
+				Console.WriteLine("Kérem a következő szövegrészt:");
+
+				szovegresz = Console.ReadLine();
+				if(szovegresz == "")
+				{
+					break;
+				}
+				osszszoveg += szovegresz;
+				osszszoveg += " ";
+			}
+			Console.WriteLine(osszszoveg);
+			Console.ReadKey();
+		}
+	}
+}
