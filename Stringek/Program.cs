7f4fe6513ab64a565a630d7195b0b1ce59302c08
János Szomor <jszomor@organicawater.com>
2020-02-25 19:15:13 +0100

    progress

--- Stringek/Program.cs
+++ Stringek/Program.cs
@@ -24,14 +24,63 @@ namespace Stringek
 				osszszoveg += szovegresz;
 				osszszoveg += " ";
 			}
-			Console.WriteLine(osszszoveg);
-
-			string[] words = osszszoveg.Split(' ');
-
-			foreach (var word in words)
+			//Console.WriteLine(osszszoveg);
+			char[] elvalaszto = new char[] { ' ' , '\t'};
+			string[] words = osszszoveg.Split(elvalaszto, StringSplitOptions.RemoveEmptyEntries);
+			int szamlalo = 0;
+			bool megvan = false;
+			//string[] q = words.Distinct().ToArray();
+			string[] egyedi = new string[words.Length];
+			for (int i = 0; i < words.Length; i++)
 			{
-				Console.WriteLine(word);
+				megvan = false;
+				for (int j = 0; j < szamlalo; j++)
+				{
+					if (words[i] == egyedi[j])
+					{
+						megvan = true;
+						break;
+					}
+				}
+				if(megvan == false)
+				{
+					egyedi[szamlalo] = words[i];
+					szamlalo++;
+				}
 			}
+			for (int i = 0; i < szamlalo; i++)
+			{
+				Console.WriteLine(egyedi[i]);
+			}
+			int counter = 0;
+			int flag = 0;
+			char[] oszChar = osszszoveg.ToCharArray();
+			for (int i = 0; i < oszChar.Length; i++)
+			{
+				if (oszChar[i] == 'L')
+					flag++;
+				else if (oszChar[i] == 'Y')
+				{
+					counter += flag;
+					if(flag == 1)
+					{
+						oszChar[i] = '?';
+						oszChar[i-1] = '?';
+					}
+					if(flag > 1)
+					{
+						oszChar[i] = '?';
+						oszChar[i - 1] = '?';
+						oszChar[i - 2] = '?';
+					}
+				}
+				else
+					flag = 0;
+			}
+
+			osszszoveg = new string(oszChar);
+			
+			//Console.WriteLine(osszszoveg);
 			Console.ReadKey();
 		}
 	}
